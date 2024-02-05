108b4017a7edaf28b8e6440ac661c56b53b3200f
Hunter <hunter.freeland2003@gmail.com>
2024-02-04 21:12:02 -0500

    redid helpers

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -6,42 +6,91 @@ exports.schemaDateToDate = (date) => {
     return today.toLocaleDateString();
 }
 
-exports.findMute = async (guildId, target) => {
+exports.findActiveInfraction = async (guildId, targetId, type) => {
     const infraction = await infractionSchema.findOne({ 
         GuildID: guildId, 
-        UserID: target.id, 
+        UserID: targetId,
         Content: {
-            $elemMatch: { Timeout: true }
+            $elemMatch: { 
+                Active: true,
+                Type: type
+            }
         }
     },
     { 'Content.$': 1 })
     if(infraction) {
         if(infraction.Content.length != 1) {
-            console.log('err')
+            console.log(`More than 1 infraction if type ${type} is active`);
             return null;
         }
     } else {
-        console.log('no mute active')
+        console.log(`No infraction of type ${type} is active`);
         return null;
     }
     return infraction.Content[0]
 }
 
-exports.updateMute = async (guildId, infraction) => {
-    const mute = await infractionSchema.findOneAndUpdate({
+exports.findInfractionByID = async (guildId, Id) => {
+    const infraction = await infractionSchema.findOne({ 
+        GuildID: guildId, 
+        Content: {
+            $elemMatch: { 
+                ID: Id,
+            }
+        }
+    },
+    { 'Content.$': 1 })
+    if(infraction) {
+        if(infraction.Content.length != 1) {
+            console.log(`More than 1 infraction if type ${type} is active`);
+            return null;
+        }
+    } else {
+        console.log(`No infraction of type ${type} is active`);
+        return null;
+    }
+    return infraction.Content[0]
+}
+
+exports.updateInfraction = async (guildId, Id) => {
+    if(!Id) {
+        console.log(`Infraction is null`);
+        return null;
+    }
+    const newInfraction = await infractionSchema.findOneAndUpdate({
         GuildID: guildId,
-        'Content.ID': infraction.ID
+        'Content.ID': Id,
     },
     {
         $set: {
-            'Content.$.Timeout': false
+            'Content.$.Active': false
         }
     },
     { returnDocument: "after" });
-    return mute;
+    return newInfraction;
 }
 
-exports.mute = async (guildId, target, user, reason, time) => {
+exports.resolveInfraction = async (guildId, Id, user) => {
+    if(!Id) {
+        console.log(`Infraction ID is null`);
+        return null;
+    }
+    const newInfraction = await infractionSchema.findOneAndUpdate({
+        GuildID: guildId,
+        'Content.ID': Id,
+    },
+    {
+        $set: {
+            'Content.$.ResolvedId': user.id,
+            'Content.$.ResolvedTag': user.tag,
+            'Content.$.Resolved': true
+        }
+    },
+    { returnDocument: "after" })
+    return newInfraction
+}
+
+exports.createInfraction = async (guildId, target, user, reason, type) => {
     const total = await infractionSchema.findOne({ GuildID: guildId, UserID: target.id }).then(async (data) => {
         const total = await totalSchema.findOne({ GuildID: guildId }).then((data) => {
             let total = 0;
@@ -62,14 +111,11 @@ exports.mute = async (guildId, target, user, reason, time) => {
             data = new infractionSchema({ 
                 GuildID: guildId,
                 UserID: target.id,
-                UserTag: target.user.tag,
                 Content: [
                     {
-                        Type: 'mute',
+                        Type: type,
                         ExecuterId: user.id,
                         ExecuterTag: user.tag,
-                        Duration: time,
-                        Timeout: true,
                         ResolvedId: null,
                         ResolvedTag: null,
                         Reason: reason,
@@ -81,11 +127,72 @@ exports.mute = async (guildId, target, user, reason, time) => {
             });
         } else {
             const warnContent = {
-                Type: 'mute',
+                Type: type,
+                ExecuterId: user.id,
+                ExecuterTag: user.tag,
+                ResolvedId: null,
+                ResolvedTag: null,
+                Reason: reason,
+                ID: total,
+                Resolved: false,
+                TimeStamp: Date.now()
+            }
+            data.Content.push(warnContent);
+        }
+        data.save();
+        return total
+    }).catch((err) => {
+        console.log(err)
+        interaction.editReply({ content: `Error` })
+        return;
+    })
+    return total;
+}
+
+exports.createTimedInfraction = async (guildId, target, user, reason, time, type) => {
+    const total = await infractionSchema.findOne({ GuildID: guildId, UserID: target.id }).then(async (data) => {
+        const total = await totalSchema.findOne({ GuildID: guildId }).then((data) => {
+            let total = 0;
+            if(!data) {
+                data = new totalSchema({
+                    GuildID: guildId
+                })
+            } else {
+                data.infractionTotal += 1;
+                data.warnTotal += 1;
+                total = data.infractionTotal
+            }
+            data.save();
+            return total;
+        })
+
+        if(!data) {
+            data = new infractionSchema({ 
+                GuildID: guildId,
+                UserID: target.id,
+                Content: [
+                    {
+                        Type: type,
+                        ExecuterId: user.id,
+                        ExecuterTag: user.tag,
+                        Duration: time,
+                        Active: true,
+                        ResolvedId: null,
+                        ResolvedTag: null,
+                        Reason: reason,
+                        ID: total,
+                        Resolved: false,
+                        TimeStamp: Date.now()
+                    }
+                ],
+            });
+        } else {
+            const warnContent = {
+                Type: type,
                 ExecuterId: user.id,
                 ExecuterTag: user.tag,
                 Duration: time,
-                Timeout: true,
+                Active: true,
                 ResolvedId: null,
                 ResolvedTag: null,
                 Reason: reason,
